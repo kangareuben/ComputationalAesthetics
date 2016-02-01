47789531f9dc00aedc9a07845a912fca28d55ddc
REUBEN BRENNER-ADAMS (RIT Student) <rmb1952@ad.rit.edu>
2016-02-01 11:50:17 -0500

    Added .eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,0
+        ],
+        "no-console": [0],
+        "no-undef":[0],
+        "quotes": [
+            2,
+            "double"
+        ],
+        "linebreak-style": [
+            1,
+            "windows"
+        ],
+        "semi": [
+            2,
+            "never"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "browser": false
+    },
+    "extends": "eslint:recommended"
+};
